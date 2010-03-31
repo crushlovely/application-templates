affc4adecdea847caece6e3ab565d2489c6cfe78
pjkelly <pj@crushlovely.com>
2010-03-30 23:05:47 -0700

    Removed Clearance-specific stuff.

--- cms.rb
+++ cms.rb
@@ -6,42 +6,17 @@
   gem 'validation_reflection', :version => '0.3.5'
   gem 'formtastic',            :version => '0.9.2'
   gem 'inherited_resources',   :version => '0.9.2'
-  gem 'will_paginate',         :version => '2.3.11'
-  gem 'clearance',             :version => '0.8.8'
+  gem 'will_paginate',         :version => '2.3.12'
+  gem 'cancan',                :version => '1.0.2'
+  gem 'authlogic',             :version => '2.1.3'
   gem 'paperclip',             :version => '2.3.1.1'
-  gem "rubypants",             :version => '0.2.0'
-  gem 'rdiscount',             :version => '1.6.3'
   gem 'acts_as_markup',        :version => '1.3.3'
 
   rake("gems:install", :sudo => true)
 
-  generate(:clearance)
-
-  route %{map.sign_in  'sign_in',
-    :controller => 'sessions',
-    :action     => 'new'
-  map.sign_out 'sign_out',
-    :controller => 'sessions',
-    :action     => 'destroy',
-    :method     => :delete
-  Clearance::Routes.draw(map)
-  }
-
-  rake "db:migrate"
-  run 'mkdir -p db/fixtures'
-  admin_pw = '123abc123'
-  file "db/fixtures/001_users.rb", %{def file_attachment(filename)
-  File.new(File.join(RAILS_ROOT, 'db', 'fixtures', "files", filename), 'rb')
-end
-
-
-password = '#{admin_pw}'
-
-User.seed(:email) do |c|
-  c.email = 'admin@crushlovely.com'
-  c.password = password
-  c.password_confirmation = password
-  c.email_confirmed = true
-end}
-  rake "db:seed_fu"
   generate(:cms_framework)
+  route %{
+    map.sign_out 'sign_out', :controller => 'user_sessions', :action => 'destroy'
+    map.sign_in 'sign_in', :controller => 'user_sessions', :action => 'new'
+    map.resources :user_sessions
+}
